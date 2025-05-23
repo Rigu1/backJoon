96c5056ec68a0e2d62a0869a4419dbb6c4e8b1ef
rigu1 <49017254+Rigu1@users.noreply.github.com>
2025-05-23 12:14:54 +0900

    [Bronze V] Title: A/B, Time: 144 ms, Memory: 9604 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,9 @@
+function divisionFromInput(input) {
+  const [num1, num2] = input.map(Number);
+  return num1 / num2;
+}
+
+if (require.main === module) {
+  const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+  console.log(divisionFromInput(input));
+}
