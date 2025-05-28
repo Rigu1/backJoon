17e3cdd9c63c50c9e0b7aef8d75dd6e8193f1753
rigu1 <49017254+Rigu1@users.noreply.github.com>
2025-05-28 19:04:15 +0900

    [Bronze V] Title: 고양이, Time: 88 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,32 @@
+const catInfo = [
+    "\\    /\\",
+    " )  ( ')",
+    "(  /  )",
+    " \\(__)|",
+]
+
+function getResult() {
+    return catInfo;
+}
+
+const print_log = (result) => {
+    if (Array.isArray(result)) { 
+        result.forEach(el => console.log(el));
+    } else { 
+        console.log(result);
+    }
+};
+
+if (require.main === module) {
+    const input = require('fs')
+        .readFileSync('/dev/stdin')
+        .toString()
+        .trim()
+        .split(/\s+/)
+        .map(Number);
+    
+    const result = getResult(input)
+    print_log(result);
+}
+
+module.exports = { getResult };
