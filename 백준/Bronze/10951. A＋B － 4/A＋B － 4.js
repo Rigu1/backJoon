5648582efe81770bcf591620feeac6cd90701a63
rigu1 <49017254+Rigu1@users.noreply.github.com>
2025-07-09 11:01:27 +0900

    [Bronze V] Title: A+B - 4, Time: 120 ms, Memory: 9792 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on('line', (line) => {
+  if (line.trim() === '') {
+    rl.close();
+  }
+  input.push(line.trim().split(/\s+/).map(Number));
+});
+
+rl.on('close', () => {
+  solution(input);
+});
+
+const solution = (input) => {
+  input.forEach(([num1, num2]) => {
+    console.log(num1 + num2);
+  })
+};
