197a276604e890c81a8beb584b3c45f78e9363e8
rigu1 <49017254+Rigu1@users.noreply.github.com>
2025-07-09 20:29:47 +0900

    [Bronze III] Title: 최소, 최대, Time: 616 ms, Memory: 135996 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on('line', (line) => {
+  if (line.trim() === '') {
+    rl.close();
+  }
+  input.push(line.trim().split(/\s+/).map(Number));
+});
+
+rl.on('close', () => {
+  solution(input);
+});
+
+const solution = ([_, numbers]) => {
+  console.log(Math.min(...numbers), Math.max(...numbers))
+};
