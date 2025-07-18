0f66d92c197cd71a3a64dbe8d08a1b7c82bf704d
rigu1 <49017254+Rigu1@users.noreply.github.com>
2025-07-18 11:27:24 +0900

    [Bronze V] Title: 아스키 코드, Time: 48 ms, Memory: 5448 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        char input = char.Parse(Console.ReadLine()!);
+        Console.WriteLine(input - 0);
+    }
+}
