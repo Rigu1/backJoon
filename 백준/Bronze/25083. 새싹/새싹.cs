00c099e2c8ea9495c418f9701696e3924a13759e
rigu1 <49017254+Rigu1@users.noreply.github.com>
2025-07-21 14:07:52 +0900

    [Bronze V] Title: 새싹, Time: 48 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Program
+{
+    static void Main()
+    {
+        string str = @"         ,r'""7
+r`-_   ,'  ,/
+ \. "". L_r'
+   `~\/
+      |
+      |";
+        Console.WriteLine(str);
+    }
+}
